6db59504710c6dfa7f6b2e93db194fdf6c11404a
kabir.faye <fayekabir21@yahoo.com>
2024-03-31 17:04:58 +0200

    updated project eks-terraform/backend.tf; git push

--- eks-terraform/backend.tf
+++ eks-terraform/backend.tf
@@ -1,7 +1,7 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "terraform-statefile-eks-dev-sec-ops-eu-central-1" // Replace with your actual S3 bucket name
-#     key    = "eks/terraform.tfstate"
-#     region = "eu-central-1" // You might have issues deploying this in N.Virginia, I'll suggest you use a different Region
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "terraform-statefile-eks-dev-sec-ops-us-east-2" // Replace with your actual S3 bucket name
+    key    = "eks/terraform.tfstate"
+    region = "us-east-2" // You might have issues deploying this in N.Virginia, I'll suggest you use a different Region
+  }
+}
